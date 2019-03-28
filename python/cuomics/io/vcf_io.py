d023c0fa665b7f25176c7d98332e506bb16c4b2c
Prasun Anand <prasunanand.bitsp@gmail.com>
2019-03-28 13:49:42 +0530

    get_gt correction

--- python/cuomics/io/vcf_io.py
+++ python/cuomics/io/vcf_io.py
@@ -11,22 +11,23 @@ def geno_read(file):
     for call in record:
       sample_id = call.sample
       gt = convert_gt_to_score(call['GT'])
-      # print(sample_id, " -> ", gt)
       gt_samples.append(gt)
     df = df.append(pd.DataFrame([[str(record.CHROM), str(record.POS), str(record.REF), str(record.ALT), gt_samples]], columns=['Chrom', 'Pos', 'Ref', 'Alt', 'GT']))
   return df
 
 def convert_gt_to_score(gt):
-  if(gt ==  '0|0' or gt ==  '0/0'):
+  # print(j)
+  jk = gt.split("|")
+  if len(jk) != 2:
+    jk = gt.split("/")
+  if(jk[0] == '.'):
     return 0
-  elif(gt ==  '0|1' or gt ==  '0/1' or gt ==  '1|0' or gt ==  '1/0'):
+  elif(jk[1] == '.'):
     return 0
-  elif(gt ==  '1|1' or gt ==  '1/1'):
-    return 0
-  else:
-    # check how to handle
-    # return 'NA'
-    return 0
+  return get_gt(int(jk[0]),int(jk[1]))
+
+def get_gt(j, k):
+  return k*(k+1)/2 + j
 
 def store_as_parquet(df):
   table = pa.Table.from_pandas(df)
